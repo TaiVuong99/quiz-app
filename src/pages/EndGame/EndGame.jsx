bfbe46124b9547a2ed0df75c7ef10b56fe4d1a55
TaiVD3 <taivd3@fsoft.com.vn>
2022-09-18 10:10:58 +0700

    te

--- src/pages/EndGame/EndGame.jsx
+++ src/pages/EndGame/EndGame.jsx
@@ -7,9 +7,7 @@ import { DataContext } from "../../components/DataProvider/DataProvider";
 
 function EndGame() {
   const db = useContext(DataContext);
-  // const [listQuestion, listAnswer, setListAnswer] = db;
-
-  console.log('hello')
+  const [listQuestion, listAnswer, setListAnswer] = db;
 
   //change to start game page
   const [startGame, setStartGame] = useState(false);
